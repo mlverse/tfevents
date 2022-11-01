d082ebd8a322c4b0b664bc89dc25d010676d1a1c
Daniel Falbel <dfalbel@gmail.com>
2022-10-31 22:27:25 -0300

    Init

---
+++ R/package.R
@@ -0,0 +1,9 @@
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @useDynLib tfevents, .registration = TRUE
+## usethis namespace: end
+NULL

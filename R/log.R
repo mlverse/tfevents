41c18a18c5c54d661342f1f0d6ad4ea041769211
Daniel Falbel <dfalbel@gmail.com>
2022-11-03 11:25:15 -0300

    Add documentation for basic functionality.

--- R/log.R
+++ R/log.R
@@ -1,3 +1,24 @@
+#' Log event
+#'
+#' @param ... Named values that you want to log. They can be possibly nested,
+#'  in this case, the enclosing names are considered 'run' names by TensorBoard.
+#' @param step The step associated the logs. If `NULL`, a managed step counter
+#'  will be used, and the global step is increased in every call to [log_event()].
+#'
+#' @note [log_event()] writes events to the default `logdir`. You can query the
+#'  default `logdir` with [get_default_logdir()] and modify it with
+#'  [set_default_logdir()]. You can also use the [with_logdir()] context switcher
+#'  to temporarily modify the logdir.
+#'
+#' @examples
+#' temp <- tempfile()
+#' with_logdir(temp, {
+#'   log_event(
+#'      train = list(loss = runif(1), acc = runif(1)),
+#'      valid = list(loss = runif(1), acc = runif(1))
+#'   )
+#' })
+#' @export
 log_event <- function(..., step = NULL) {
   data <- rlang::dots_list(..., .named = TRUE, .homonyms = "error")
   step <- get_global_step()
@@ -52,14 +73,35 @@ write_event.numeric <- function(data, name, step) {
 
 .tfevents <- new.env()
 
+#' Query and modify the logdir
+#'
+#' [log_event()] has a notion of default logdir, so you don't need to specify it
+#' at every call. These functions allow you to query and the current logdir.
+#'
+#' @param logdir The `logdir` that you want to set as default.
+#' @param new The `logdir` that you want to set as default, used in [with_logdir()].
+#' @param code Expressions that will be evaluated in a context with the `new`
+#'   `logdir` as the default `logdir`.
+#'
+#' @examples
+#' temp <- tempfile()
+#' get_default_logdir()
+#' with_logdir(temp, {
+#'  print(get_default_logdir())
+#' })
+#' @export
 get_default_logdir <- function() {
   if (is.null(.tfevents$logdir))
     set_default_logdir()
   rlang::env_get(.tfevents, "logdir")
 }
+#' @describeIn get_default_logdir Modifies the default `logdir`.
+#' @export
 set_default_logdir <- function(logdir = "logs") {
   rlang::env_bind(.tfevents, logdir = path.expand(logdir))
 }
+#' @describeIn get_default_logdir Temporarily modify the default `logdir`.
+#' @export
 with_logdir <- withr::with_(
   set = set_default_logdir,
   get = function(logdir) {
@@ -67,7 +109,24 @@ with_logdir <- withr::with_(
   }
 )
 
-.steps <- new.env()
+#' Global step counters
+#'
+#' @param step New value for `step`.
+#'
+#' @details `tfevents` tracks and automatically increased the step counter whenever
+#' [log_event()] is called. Note that, it maintains a separate step counter for
+#' each root `logdir`, thus if you change the `logdir` using [set_default_logdir()]
+#' or [with_logdir()], a different step counter will be used.
+#'
+#' @examples
+#' temp <- tempfile()
+#' with_logdir(temp, {
+#'   print(get_global_step())
+#'   set_default_step(100)
+#'   print(get_global_step())
+#' })
+#' print(get_global_step())
+#' @export
 get_global_step <- function() {
   # a separate global step count is tracked for each root logdir.
   # the global step is queried once when calling `log_event`.
@@ -78,9 +137,12 @@ get_global_step <- function() {
   set_global_step(cur_step)
   cur_step
 }
+#' @describeIn get_global_step Set the global step.
+#' @export
 set_global_step <- function(step) {
   rlang::env_poke(.steps, get_default_logdir(), as.integer(step), create = TRUE)
 }
+.steps <- new.env()
 
 .writers <- new.env()
 get_writer <- function(logdir = get_default_logdir()) {

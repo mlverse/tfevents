dd94b712a46357481e8930c361f776ccd1d8120c
Daniel Falbel <dfalbel@gmail.com>
2022-11-08 21:39:21 -0300

    Add test with the recommended strategy of running each run a separate directory.

--- tests/testthat/test-hparams.R
+++ tests/testthat/test-hparams.R
@@ -44,3 +44,45 @@ test_that("simple hparams experiment", {
   expect_equal(nrow(collect_scalars(temp)), 30)
 })
 
+test_that("multiple runs, each in a different logdir", {
+
+  hparams <- list(
+    hparams_hparam("dropout", domain = c(min_value = 0.1, max_value = 0.5)),
+    hparams_hparam("optimizer", domain = c("adam", "sgd")),
+    hparams_hparam("use_cnn", domain = c(TRUE, FALSE)),
+    hparams_hparam("num_units", c(8, 12, 16))
+  )
+
+  metrics <- list(
+    hparams_metric(tag = "loss"),
+    hparams_metric(tag = "accuracy", group = "valid"),
+    hparams_metric(tag = "f1", dataset_type = "training")
+  )
+
+  temp <- tempfile()
+
+  with_logdir(temp, {
+    hparams_config(hparams, metrics)
+  })
+
+  for(run in 1:10) {
+    with_logdir(file.path(temp, paste0("run", run)), {
+      hparams_hparams(hparams = list(
+        dropout = runif(1, min = 0.1, max = 0.5),
+        optimizer = sample(c("adam", "sgd"), 1),
+        use_cnn = sample(c(TRUE, FALSE), 1),
+        num_units = sample(c(8, 12, 16), 1)
+      ))
+
+      for (i in 1:10) {
+        log_event(loss = runif(1), valid = list(accuracy = runif(1)),
+                  f1 = runif(1))
+      }
+    })
+  }
+
+  reader <- tbparse$SummaryReader(temp)
+  expect_equal(nrow(reader$hparams), 40)
+  expect_equal(nrow(collect_events(file.path(temp, "run1"))), 30 + 3)
+})
+

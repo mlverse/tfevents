c0a1e49b2c2fc862e06d7d051a079722a075d5ce
Daniel Falbel <dfalbel@gmail.com>
2022-11-25 15:19:20 -0300

    Add tests for audio writing.

---
+++ R/audio.R
@@ -0,0 +1,40 @@
+#' Summary audio
+#'
+#' Audio summaries can be played withing the TensorBoard UI.
+#'
+#' @param audio Object that will be written as an audio event in the tfevents record.
+#' @inheritParams summary_image
+#' @param sample_rate The sample rate in Hz associated to the audio values.
+#'
+#' @export
+summary_audio <- function(audio, ..., metadata = NULL, tag = NA) {
+  UseMethod("summary_audio")
+}
+
+#' @describeIn summary_audio Creates a summary from a 3D array with dimensions
+#'   `(batch_size, n_samples, n_channels)`. Values must be in the range `[-1, 1]`.
+#' @export
+summary_audio.array <- function(audio, ..., sample_rate = 44100, metadata = NULL,
+                          tag = NA) {
+
+  rlang::check_installed("wav")
+  if (is.null(metadata)) {
+    metadata <- summary_metadata(plugin_name = "audio")
+  }
+
+  temp <- tempfile()
+  raw_audios <- apply(audio, 1, simplify = FALSE, function(x) {
+    wav::write_wav(t(x), sample_rate = sample_rate, path = temp)
+    sze <- fs::file_info(temp)$size
+    readBin(temp, n = sze, what = "raw")
+  })
+
+  blob_audios <- blob::new_blob(raw_audios)
+
+  summary_tensor(
+    blob_audios,
+    dtype = "string",
+    metadata = metadata,
+    tag = tag
+  )
+}

e44ec3ed624a50a86ad42a8780fda49c312cb45c
Daniel Falbel <dfalbel@gmail.com>
2026-04-27 08:01:52 -0300

    Only py_require tbparse in test helper, not tensorflow
    Loading tensorflow during R CMD check creates ~/.keras/keras.json
    which violates CRAN policy. Tests only need tbparse.

--- tests/testthat/helper-tbparse.R
+++ tests/testthat/helper-tbparse.R
@@ -2,7 +2,7 @@ skip_if_tbparse_not_available <- function() {
   skip_if(inherits(try(reticulate::import("tbparse"), silent = TRUE), "try-error"))
 }
 
-reticulate::py_require(c("tbparse", "tensorflow", "tensorboard"))
+reticulate::py_require("tbparse")
 
 if (inherits(try(reticulate::import("tbparse"), silent = TRUE), "try-error")) {
   tbparse <- NULL

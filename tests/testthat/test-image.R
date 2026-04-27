2544ee5cffd3cf00d1a48e5e572ec50cf3a34eb6
Daniel Falbel <dfalbel@gmail.com>
2026-04-27 08:23:19 -0300

    Skip tensorflow-dependent test when not available, restore tbparse CI install
    - Add skip for tensorflow in test-image.R line 19
    - Restore py_install('tbparse') in check and test-coverage workflows

--- tests/testthat/test-image.R
+++ tests/testthat/test-image.R
@@ -12,6 +12,8 @@ test_that("write image", {
 
 
   skip_if_tbparse_not_available()
+  skip_if_not_installed("tensorflow")
+  skip_if(inherits(try(reticulate::import("tensorflow"), silent = TRUE), "try-error"))
   reader <- tbparse$SummaryReader(temp)
   buf <- reader$tensors$value[[1]][[3]]
   # TODO: in theory we don't need tensorflow for this, but couldnt find a way to
